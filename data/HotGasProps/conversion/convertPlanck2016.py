Added conversion scripts for Rasmussen and BAHAMAS data. Still missing reference for Rasmussen data. Fixed error bar calculation in other hot gas conversion scripts.

data/HotGasProps/conversion/convertPlanck2016.py
@@ -19,8 +19,11 @@ if not os.path.exists(output_directory):
 
 data = np.loadtxt(input_filename, skiprows=1, delimiter=",")
 x = unyt.unyt_array(10.0 ** data[:, 0], unyt.Msun)
-y = unyt.unyt_array(data[:, 1], unyt.Mpc ** 2)
-yerr = unyt.unyt_array(data[:, 2:].T, unyt.Mpc ** 2)
+y = unyt.unyt_array(10.0 ** data[:, 1], unyt.Mpc ** 2)
+yerr = np.zeros((2, data.shape[0]))
+yerr[0, :] = 10.0 ** (data[:, 1] - data[:, 2])
+yerr[1, :] = 10.0 ** (data[:, 3] - data[:, 1])
+yerr = unyt.unyt_array(yerr, unyt.Mpc ** 2)
 
 # Meta-data
 comment = "No comment"
@@ -39,7 +42,7 @@ processed.associate_y(
     y,
     scatter=yerr,
     comoving=True,
-    description="$\mathrm{log}_{10} (\mathrm{E}(z)^{-2/3} Y_\mathrm{SZ, hse}(<5r_{500}) D_A^2$",
+    description="$\mathrm{E}(z)^{-2/3} Y_\mathrm{SZ, hse}(<5r_{500}) D_A^2$",
 )
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)

data/HotGasProps/conversion/convertRasmussen_entropy_profiles.py
@@ -9,10 +9,10 @@ import sys
 with open(sys.argv[1], "r") as handle:
     exec(handle.read())
 
-input_filename = "../raw/Sun09_entropy_profiles.txt"
+input_filename = "../raw/Rasmussen_entropy_profiles.txt"
 
 output_directory = "../"
-output_filename = "Sun09_entropy_profiles.hdf5"
+output_filename = "RasmussenYY_entropy_profiles.hdf5"
 
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
@@ -23,16 +23,13 @@ y = unyt.unyt_array(data[:, 1], unyt.dimensionless)
 yerr = np.zeros((2, data.shape[0]))
 yerr[0, :] = data[:, 1] - data[:, 2]
 yerr[1, :] = data[:, 3] - data[:, 1]
-yerr = np.zeros((2, data.shape[0]))
-yerr[0, :] = 10.0 ** (data[:, 1] - data[:, 2])
-yerr[1, :] = 10.0 ** (data[:, 3] - data[:, 1])
 yerr = unyt.unyt_array(yerr, unyt.dimensionless)
 
 # Meta-data
 comment = "No comment"
-citation = "Sun et al. (2009)"
-bibcode = "2009ApJ...693.1142S"
-name = "Entropy profiles for 43 nearby galaxy groups"
+citation = "Rasmussen et al. (??)"
+bibcode = "unknown"
+name = "Entropy profiles for ??"
 plot_as = "points"
 redshift = 0.0
 

data/HotGasProps/conversion/convertSun2009_ne_profiles.py
@@ -21,7 +21,10 @@ data = np.loadtxt(input_filename)
 # we discard the last two rows, since they are outside r500
 x = unyt.unyt_array(10.0 ** data[:-2, 0], unyt.dimensionless)
 y = unyt.unyt_array(10.0 ** data[:-2, 1], unyt.cm ** (-3))
-yerr = unyt.unyt_array(10.0 ** data[:-2, 2:].T, unyt.cm ** (-3))
+yerr = np.zeros((2, data.shape[0]-2))
+yerr[0, :] = 10.0 ** (data[:-2, 1] - data[:-2, 2])
+yerr[1, :] = 10.0 ** (data[:-2, 3] - data[:-2, 1])
+yerr = unyt.unyt_array(yerr, unyt.cm ** (-3))
 
 # Meta-data
 comment = "No comment"

data/HotGasProps/conversion/convertSun2011_P_profiles.py
@@ -20,7 +20,10 @@ if not os.path.exists(output_directory):
 data = np.loadtxt(input_filename)
 x = unyt.unyt_array(10.0 ** data[:, 0], unyt.dimensionless)
 y = unyt.unyt_array(10.0 ** data[:, 1], unyt.dimensionless)
-yerr = unyt.unyt_array(10.0 ** data[:, 2:].T, unyt.dimensionless)
+yerr = np.zeros((2, data.shape[0]))
+yerr[0, :] = 10.0 ** (data[:, 1] - data[:, 2])
+yerr[1, :] = 10.0 ** (data[:, 3] - data[:, 1])
+yerr = unyt.unyt_array(yerr, unyt.dimensionless)
 
 # Meta-data
 comment = "No comment"