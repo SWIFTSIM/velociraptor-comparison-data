f4b5d5b7a90123f7ddaf8302eff3f1e49b413377
EvgeniiChaikin <bowowoda@gmail.com>
2023-11-06 13:38:01 +0100

    switch cosmology from Planck2015 to 3x2pt + all ext. from Abbott et al. (2022)

--- cosmology.py
+++ cosmology.py
@@ -1,4 +1,9 @@
-from astropy.cosmology import Planck15 as cosmology
+from astropy.cosmology import FlatLambdaCDM
+
+# Cosmology from table II (3x2pt + all external constraints) from T. Abbott et al. (2022)
+cosmology = FlatLambdaCDM(
+    H0=0.681e2, Om0=0.306, Ob0=0.0486, m_nu=[0.0, 0.0, 0.06], Tcmb0=2.7255
+)
 
 # Asplund et al. (2009)
 solar_metallicity = 0.0134
